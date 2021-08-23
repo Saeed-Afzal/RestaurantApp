a439b5a99e43fc1ea55469031f2f29ae891839d6
Saeed-Afzal <saeedafzal429@gmail.com>
2021-08-23 23:52:42 +0500

    Hackathon

---
+++ conn/conn.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBmt75BbgU9vUjcv_cuJC2BuxdY3ChOB-Q",
+  authDomain: "restaurant-app-b0471.firebaseapp.com",
+  databaseURL: "https://restaurant-app-b0471-default-rtdb.firebaseio.com",
+  projectId: "restaurant-app-b0471",
+  storageBucket: "restaurant-app-b0471.appspot.com",
+  messagingSenderId: "32415566805",
+  appId: "1:32415566805:web:6a952d75cb0f4f93d94b5c",
+  measurementId: "G-2MR0LKL644"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
